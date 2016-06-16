1ba355b83b7e11e3c0147f116994c3fe570c5d10
bill.lawrence <willizm.cn@gmail.com>
2016-06-16 22:54:29 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    define('APP_ID', 'wxd442f56b691467da');
+    define('APP_SECRET', '00718cd2452948837941ba1b1cc9331c');
+    define('DATA_PATH', './data/');
+    /*
+     * Environment
+     */
+    //define('ENV', 'sae');
+    define('ENV', 'dev');
+    //define('SAE_DATA_PATH', 'saekv://');
